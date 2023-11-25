06de8b7b8d23758eb2351018ed5f0a506195fcf8
Donat <donat.oshchepkov@gmail.com>
2023-11-25 16:35:41 +0300

    add testCircle with exception handler

---
+++ OOP_2/src/TestCircleWithException/CircleWithException.java
@@ -0,0 +1,45 @@
+package TestCircleWithException;
+
+public class CircleWithException {
+    /** Радиус круга */
+    private double radius;
+
+    /** Количество созданных объектов */
+    private static int numberOfObjects = 0;
+
+    /** Создает круг с радиусом, равным 1 */
+    public CircleWithException() {
+        this(1.0);
+    }
+
+    /** Создает круг с указанным радиусом */
+    public CircleWithException(double newRadius) {
+        setRadius(newRadius);
+        numberOfObjects++;
+    }
+
+    /** Возвращает радиус */
+    public double getRadius() {
+        return radius;
+    }
+
+    /** Присваивает новый радиус */
+    public void setRadius(double newRadius)
+            throws IllegalArgumentException {
+        if (newRadius >= 0)
+            radius = newRadius;
+        else
+            throw new IllegalArgumentException(
+                    "Радиус не может быть отрицательным");
+    }
+
+    /** Возвращает numberOfObjects */
+    public static int getNumberOfObjects() {
+        return numberOfObjects;
+    }
+
+    /** Возвращает площадь круга */
+    public double findArea() {
+        return radius * radius * 3.14159;
+    }
+}
