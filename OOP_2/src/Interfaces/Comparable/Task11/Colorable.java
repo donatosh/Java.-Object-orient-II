287f0dc325447e91b10bcda272bd47a60b7e12f2
Donat Oshchepkov <donat@MacBook-Pro-Donat.local>
2023-12-11 21:15:24 +0300

    Add 11, 12 tasks from Interfaces course

---
+++ OOP_2/src/Interfaces/Comparable/Task11/Colorable.java
@@ -0,0 +1,5 @@
+package Interfaces.Comparable.Task11;
+
+public interface Colorable {
+    public void howToColor();
+}
---
+++ OOP_2/src/Interfaces/Comparable/Task12/TestMyStack.java
@@ -0,0 +1,21 @@
+package Interfaces.Comparable.Task12;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class TestMyStack {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(new BigDecimal(1));
+        stack.push(new BigDecimal(2));
+        stack.push(new BigDecimal(3));
+        System.out.println(stack.hashCode());
+        System.out.println(stack.getList().hashCode());
+
+        MyStack stackClone = new MyStack();
+        stackClone = (MyStack) stack.clone();
+        stackClone.push(new BigDecimal(4));
+        System.out.println(stackClone.hashCode());
+        System.out.println(stackClone.getList().hashCode());
+    }
+}
